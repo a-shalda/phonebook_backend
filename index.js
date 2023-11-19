69a59dc35398a878edad18143d50f430eba66b79
Alex Shalda <123298731+a-shalda@users.noreply.github.com>
2023-11-19 10:49:04 +0400

    3.2

--- index.js
+++ index.js
@@ -29,6 +29,16 @@ app.get('/api/persons', (request, response) => {
   response.send(persons)
 })
 
+app.get('/info', (request, response) => {
+  const d = new Date()
+
+  const data = `
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${d}</p>
+  `
+  response.send(data)
+})
+
 app.get('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
   const note = persons.find(note => note.id === id)

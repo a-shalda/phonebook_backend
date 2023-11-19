b9b120325d59f3befa2fdbc81919918c3b72fff2
Alex Shalda <123298731+a-shalda@users.noreply.github.com>
2023-11-19 10:52:50 +0400

    3.3

--- index.js
+++ index.js
@@ -41,10 +41,10 @@ app.get('/info', (request, response) => {
 
 app.get('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
-  const note = persons.find(note => note.id === id)
+  const person = persons.find(person => person.id === id)
   
-  if (note) {
-    response.json(note)
+  if (person) {
+    response.send(person)
   } else {
     response.status(404).end()
   }
